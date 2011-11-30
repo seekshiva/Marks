c2bf9abd561917600a402f03875fb01ff00e47c4
Shiva Nandan <seekshiva@gmail.com>
2011-11-30 23:40:53 +0530

    forgot to add this file to the last commit.

---
+++ signatures.php
@@ -0,0 +1,11 @@
+<div class="op s" style="margin-top:100px; text-align:center;">
+<div style="float:left; ">
+Exam Controller
+</div>
+<div style="float:right; ">
+Secretary
+</div>
+<div>
+Principal
+</div>
+</div>
